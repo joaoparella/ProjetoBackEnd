cab24a0e16a643ebf0d37776b233ba9f32bb6a97
Joao <joao.pparella@senacsp.edu.br>
2025-02-18 20:20:23 -0300

    Alterações dia 18/02/2025 - encapsulamento

--- Principal.js
+++ Principal.js
@@ -3,22 +3,28 @@ import filme from "./filme.js";
 import serie from "./serie.js";
 import { Usuario } from "./Usuario.js";
 
-// const usuario1 = new Usuario("Roberval","Roberval@bol.com","senhadificil123")
+const usuario1 = new Usuario("Roberval","Roberval@bol.com","senhaDificil123")
 const ES = new funcoesES();
 
-// usuario1.nome= "roberval da silva"
+usuario1.nome= "roberval da silva"
 
 // var login = ES.entrada("Digite o usuário:")
 // var senha = ES.entrada("Digite a senha:")
 
-// ES.mensagemCompleta(usuario1.fazerLogin(login,senha))
+usuario1.trocarSenha("Senha1234")
+
+usuario1.senha = "TestandoNovaSenha123"
+// var teste = usuario1.senha;
+
+
+ES.mensagemCompleta(teste)
 
 // ES.mensagemCompleta(usuario1.validarAssinatura())
 // usuario1.adicionarAssinatura(10)
 // ES.mensagemCompleta(usuario1.validarAssinatura())
 
-const filme1 = new filme('Harry potter',180,'Um bruxo vai para a escola',2006);
-const serie1 = new serie('Episodio piloto',30,'A historia de um MC',2021,'Sintonia','01','01')
+// const filme1 = new filme('Harry potter',180,'Um bruxo vai para a escola',2006);
+// const serie1 = new serie('Episodio piloto',30,'A historia de um MC',2021,'Sintonia','01','01')
 
-ES.mensagemCompleta(filme1.compartilhar())
-ES.mensagemCompleta(serie1.compartilhar())
+// ES.mensagemCompleta(filme1.compartilhar())
+// ES.mensagemCompleta(serie1.compartilhar())
--- Usuario.js
+++ Usuario.js
@@ -1,17 +1,25 @@
 import { Datas } from "./datas.js"; 
 
 export class Usuario{
+    #senha
     constructor(nome,email,senha){
         this.datas = new Datas();
         this.nome = nome;
         this.email = email;
-        this.senha = senha;
+        var resultadoSenha = this.trocarSenha(senha)
         this.assinatura = this.datas.dataAtual();
+        return resultadoSenha;
+    }
+
+    
+
+    set senha(senhaNova){
+        this.trocarSenha(senhaNova);
     }
 
     fazerLogin(email,senha){
         if(email == this.email){
-            if(senha == this.senha){
+            if(senha == this.#senha){
                 return "Login efetuado com sucesso"
             }else{
                 return "Login inválido" 
@@ -19,6 +27,20 @@ export class Usuario{
         }else{
             return "Login inválido"
         }
+    }
+
+    trocarSenha(senhaNova){
+        var tamanho = senhaNova.length >= 8;
+        var temNumeros = /\d/.test(senhaNova)
+        var temMaiusculo = /[A-Z]/.test(senhaNova)
+        var temMinusculo = /[a-z]/.test(senhaNova)
+        if(tamanho && temMaiusculo && temMinusculo && temNumeros){
+            this.#senha = senhaNova;
+            return "Senha atualizada"
+        }else{
+            return "Senha fraca"
+        }
+        
     }
     
     validarAssinatura(){
